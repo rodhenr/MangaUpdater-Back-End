7e10941016486e4e79c931d2e158be69aaf43a29
Rodrigo Henrique <rodhenrc@outlook.com>
2022-09-29 20:18:39 -0300

    :sparkles: feat: Novo metodo searchManga adicionado, nova rota Search adicionada

--- src/routes/manga.route.ts
+++ src/routes/manga.route.ts
@@ -3,12 +3,13 @@ import {
   newRegister,
   updateRegister,
 } from "../controllers/commands.controller";
-import { followingData } from "../controllers/user.controller";
+import { followingData, searchManga } from "../controllers/user.controller";
 
 const mangaRoutes = Express.Router();
 
 mangaRoutes.route("/api/new").post(newRegister);
 mangaRoutes.route("/api/update").post(updateRegister);
 mangaRoutes.route("/api/list").post(followingData);
+mangaRoutes.route("/api/search").post(searchManga)
 
 export default mangaRoutes;
